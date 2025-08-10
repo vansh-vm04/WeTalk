48cc6b73cb6e2928ca431cb2ad1e12575cfc20c3
Vansh <vanshmorwani1@gmail.com>
2025-08-11 03:02:07 +0530

    added analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import JoinRoom from "./pages/JoinRoom"
 import Chat from "./pages/Chat"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Route path="/" element={<JoinRoom/>}/>
     <Route path="/chat" element={<Chat/>}/>
     </Routes>
+    <Analytics/>
     </div>
   )
 }
