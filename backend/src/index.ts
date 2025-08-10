b6842b1bd8b4c15a1fee1e7591db1acb739f89c2
Vansh <vanshmorwani1@gmail.com>
2025-08-11 00:17:36 +0530

    fixed some bugs in backend added logic re-joining in same room

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,38 +6,40 @@ let onlineUsers: number = 0;
 
 interface UserType {
   username: string;
-  type: "join" | "create" | "chat" | "re-join";
+  type: "join" | "create" | "chat" | "re-join" | "leave";
   roomId: string;
   socket: WebSocket;
-  message: string;
+  message?: string;
 }
 
-let allSockets: Map<string, UserType[]> = new Map<string, UserType[]>();
+let allRooms: Map<string, UserType[] | []> = new Map<string, UserType[]>();
+
+const isOpen = (socket: WebSocket) => {
+  return socket.readyState == WebSocket.OPEN;
+};
 
 const userExists = (arr: UserType[], username: string): boolean => {
-  arr.forEach((u) => {
-    if (u.username === username) return true;
-  });
-  return false;
+  return arr.some((u) => u.username == username);
 };
 
 const removeUser = (arr: UserType[], username: string) => {
-   const newUsers = arr.filter((u)=>u.username != username);
-   return newUsers;
+  const newUsers = arr.filter((u) => u.username != username);
+  return newUsers;
 };
 
 wss.on("connection", (socket: WebSocket) => {
   socket.on("message", async (message: string) => {
     const data: UserType = await JSON.parse(message);
     data.socket = socket;
+
     if (data.type == "create") {
-      if (allSockets.has(data.roomId)) {
+      if (allRooms.has(data.roomId)) {
         socket.send("Room already exists");
         console.log("Room already exists");
         return;
       }
       let users: UserType[] = [data];
-      allSockets.set(data.roomId, users);
+      allRooms.set(data.roomId, users);
       onlineUsers++;
       socket.send("Joined with room Id : " + data.roomId);
       console.log(
@@ -49,18 +51,18 @@ wss.on("connection", (socket: WebSocket) => {
     }
 
     if (data.type == "join") {
-      if (!allSockets.has(data.roomId)) {
+      if (!allRooms.has(data.roomId)) {
         socket.send("Room not found");
         return;
       }
 
-      let users: UserType[] = allSockets.get(data.roomId)!;
+      let users: UserType[] = allRooms.get(data.roomId)!;
       if (userExists(users, data.username)) {
         socket.send("Username already present in room");
         return;
       }
       users.push(data);
-      allSockets.set(data.roomId, users);
+      allRooms.set(data.roomId, users);
       onlineUsers++;
       socket.send("Joined room : " + data.roomId);
       console.log(
@@ -72,32 +74,77 @@ wss.on("connection", (socket: WebSocket) => {
     }
 
     if (data.type == "chat") {
-      let users: UserType[] = allSockets.get(data.roomId)!;
+      let users: UserType[] = allRooms.get(data.roomId)!;
       users.forEach((u) => {
-        if (u.username != data.username) {
+        if (u.socket != socket && isOpen(u.socket)) {
           u.socket.send(data.username + ": " + data.message);
         }
       });
     }
 
     if (data.type == "re-join") {
-      if (!allSockets.has(data.roomId)) {
+      if (!allRooms.has(data.roomId)) {
         socket.send("Room Expired");
-        console.log('A user tried to join expired room')
+        console.log("A user tried to join expired room");
         return;
       }
-      let users: UserType[] = allSockets.get(data.roomId)!;
-      let newUsers = removeUser(users,data.username);
-      newUsers.push(data);
-      console.log(newUsers);
-      allSockets.set(data.roomId, newUsers);
+      let users: UserType[] = allRooms.get(data.roomId)!;
+      users.push({
+        username: data.username,
+        type: "re-join",
+        roomId: data.roomId,
+        socket: socket,
+      });
+      allRooms.set(data.roomId, users);
       socket.send("Joined room : " + data.roomId);
+      users.forEach((u) => {
+        if (isOpen(u.socket))
+          u.socket.send(`${data.username} rejoined the room`);
+      });
+      onlineUsers++;
       console.log(
         "A user rejoined room : " +
           data.roomId +
           ". Current online users: " +
           onlineUsers
       );
+    }
+
+    if (data.type == "leave") {
+      if (!allRooms.has(data.roomId)) {
+        return;
+      }
+      let users = allRooms.get(data.roomId)!;
+      const index = users.findIndex((u) => u.socket === socket);
+      if (index != -1) {
+        let username = users[index].username;
+        users.splice(index, 1);
+        if (users.length == 0) allRooms.delete(data.roomId);
+        users.forEach((u) => {
+          if (isOpen(u.socket)) {
+            u.socket.send(`${username} left the room`);
+          }
+        });
+        onlineUsers--;
+      }
+    }
+  });
+
+  socket.on("close", () => {
+    if (allRooms.size == 0) return;
+    for (const [roomId, users] of allRooms.entries()) {
+      const index = users.findIndex((u) => u.socket === socket);
+      if (index != -1) {
+        let username = users[index].username;
+        users.splice(index, 1);
+        if (users.length == 0) allRooms.delete(roomId);
+        users.forEach((u) => {
+          if (isOpen(u.socket)) {
+            u.socket.send(`${username} left the room`);
+          }
+        });
+        onlineUsers--;
+      }
     }
   });
 });
